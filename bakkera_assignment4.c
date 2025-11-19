4cd91953d99dbecfb77c33cf51cff284b36bf40f
Allessandra <bakkera@oregonstate.edu>
2025-11-18 17:38:23 -0800

    Finished part 6

--- bakkera_assignment4.c
+++ bakkera_assignment4.c
@@ -107,7 +107,21 @@ int main()
 			if (child_pid == -1) {
 				perror("fork");
 				last_status = 1;
-			} else if (child_pid == 0) {				
+			} else if (child_pid == 0) {	
+				if (curr_command->is_bg) {
+					if (!curr_command->input_file) {
+						int fd_in = open("/dev/null", O_RDONLY);
+						dup2(fd_in, 0);
+						close(fd_in);
+					} 
+					
+					if (!curr_command->output_file) {
+						int fd_out = open("/dev/null", O_WRONLY);
+						dup2(fd_out, 1);
+						close(fd_out);
+					}
+				}
+				// Input redirection
 				if (curr_command->input_file) {
 					int fd_in = open(curr_command->input_file, O_RDONLY);
 					if (fd_in == -1) {
@@ -119,7 +133,7 @@ int main()
 					}
 						close(fd_in);
 				}
-
+				
 				// Output redirection
 				if (curr_command->output_file) {
 					int fd_out = open(curr_command->output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
@@ -141,16 +155,30 @@ int main()
 
 					int child_status;
 					waitpid(child_pid, &child_status, 0);
+					
 					if (WIFEXITED(child_status)) {
 						last_status = WEXITSTATUS(child_status);
 					} else if (WIFSIGNALED(child_status)) {
 						printf("terminated by signal %d\n", WTERMSIG(child_status));
+						fflush(stdout);
 						last_status = WTERMSIG(child_status);
 					}
 				} else {
 					printf("background pid is %d\n", child_pid);
 				}
 			}
+		}
+
+		int child_status;
+		pid_t finished_pid;
+
+		while ((finished_pid = waitpid(-1, &child_status, WNOHANG)) > 0) {
+			if (WIFEXITED(child_status)) {
+				printf("background pid %d is done: exit value %d\n", finished_pid, WEXITSTATUS(child_status));
+			} else if (WIFSIGNALED(child_status)) {
+				printf("background pid %d is done: terminated by signal %d\n", finished_pid, WTERMSIG(child_status));
+			}
+			fflush(stdout);
 		}
 	
 
